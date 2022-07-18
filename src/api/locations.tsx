3795b54538c443001b6d74cc9f15588e7c46b9ec
Alex Kotliarov <slicer256@gmail.com>
2022-07-19 00:17:55 +0200

    show locations suggestions when typing

--- src/api/locations.tsx
+++ src/api/locations.tsx
@@ -2,7 +2,7 @@ import client from './client';
 
 export const searchLocation = (query: string) => {
   return client.get(
-    'https://efastatic.vvs.de/umweltrechner/XML_STOPFINDER_REQUEST? outputFormat=rapidJSON&SpEncId=0&coordOutputFormat=EPSG:4326&serverInfo=1&suggestApp=vvs&t ype_sf=any&version=10.2.10.139',
+    'https://efastatic.vvs.de/umweltrechner/XML_STOPFINDER_REQUEST?outputFormat=rapidJSON&SpEncId=0&coordOutputFormat=EPSG:4326&serverInfo=1&suggestApp=vvs&type_sf=any&version=10.2.10.139',
     {
       name_sf: query
     }
--- src/components/common/LocationInput.tsx
+++ src/components/common/LocationInput.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { stopIcon } from '../../config/icons';
 import useApi from '../../hooks/useApi';
 import { searchLocation } from '../../api/locations';
 
@@ -8,15 +9,65 @@ interface Props {
   style?: React.CSSProperties;
 }
 
+interface ILocation {
+  id: string;
+  disassembledName: string;
+  matchQuality: number;
+}
+
 export default function SearchInput({ placeholder, style }: Props) {
   const searchLocationsApi = useApi(searchLocation);
+  const [suggestions, setSuggestions] = useState([]);
+  const [query, setQuery] = useState('');
 
-  useEffect(() => {
-    searchLocationsApi.request();
-  }, []);
+  const change = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    const data = await searchLocationsApi.request(e.target.value);
 
-  return <Input style={style} type="text" placeholder={placeholder} />;
+    // sort by best match
+    console.log(data.locations);
+    data.locations.sort(
+      (l1: ILocation, l2: ILocation) => l2.matchQuality - l1.matchQuality
+    );
+    console.log(data.locations);
+
+    // prepare suggestions list and limit it
+    setSuggestions(
+      data.locations
+        .map((location: ILocation) => location.disassembledName)
+        .slice(0, 20)
+    );
+  };
+
+  return (
+    <Container style={style}>
+      <Input
+        type="text"
+        value={query}
+        onChange={change}
+        placeholder={placeholder}
+      />
+      {suggestions && (
+        <Suggestions>
+          {suggestions.map((suggestion: string, index: number) => (
+            <SuggestionsItem key={index}>
+              <img
+                src={stopIcon}
+                width="16"
+                style={{ marginRight: 12 }}
+                alt=""
+              />
+              {suggestion}
+            </SuggestionsItem>
+          ))}
+        </Suggestions>
+      )}
+    </Container>
+  );
 }
+const Container = styled.div`
+  width: 100%;
+`;
 
 const Input = styled.input`
   border: 0;
@@ -31,5 +82,26 @@ const Input = styled.input`
 
   &:focus {
     outline: none;
+  }
+`;
+
+const Suggestions = styled.div`
+  position: absolute;
+  background-color: white;
+  border-radius: 8px;
+  border: 1px solid #eee;
+  padding: 0 16px;
+  z-index: 5;
+`;
+
+const SuggestionsItem = styled.div`
+  display: flex;
+  cursor: pointer;
+  padding: 5px 10px;
+  line-height: 20px;
+  border-bottom: 1px solid #eee;
+
+  &:hover {
+    background-color: #efefef;
   }
 `;
